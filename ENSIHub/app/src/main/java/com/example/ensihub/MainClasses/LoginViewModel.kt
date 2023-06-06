7de11b75b1c61080e342f393e438d9756e35a483
Tartiine <sofia.saadi6@gmail.com>
2023-06-06 19:32:34 +0200

    Fixing text input not displayed on the authentication view

--- ENSIHub/app/src/main/java/com/example/ensihub/MainClasses/LoginViewModel.kt
+++ ENSIHub/app/src/main/java/com/example/ensihub/MainClasses/LoginViewModel.kt
@@ -1,10 +1,10 @@
 package com.example.ensihub.MainClasses
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.ensihub.MainClasses.AuthRepository
@@ -13,99 +13,109 @@ import kotlinx.coroutines.launch
 class LoginViewModel(
     private val repository: AuthRepository = AuthRepository()
 ): ViewModel() {
+    val loginUiState: MutableState<LoginUiState> = mutableStateOf(LoginUiState())
     val currentUser = repository.currentUser
-
+    val userName: MutableState<String> = mutableStateOf("")
+    val password: MutableState<String> = mutableStateOf("")
     val hasUser:Boolean
         get() = repository.hasUser()
 
-    var loginUiState by mutableStateOf(LoginUiState())
-        private set
-
-    fun onUserNameChange(userName:String){
-        loginUiState = loginUiState.copy(userName = userName)
+    fun onUserNameChange(newUserName: String) {
+        Log.d("LoginViewModel", "onUserNameChange called with: $newUserName")
+        loginUiState.value = loginUiState.value.copy(userName = newUserName)
     }
 
     fun onPasswordChange(password:String){
-        loginUiState = loginUiState.copy(password = password)
+        Log.d("LoginViewModel", "onPasswordChange called with: $password")
+        loginUiState.value = loginUiState.value.copy(password = password)
     }
 
     fun onUserNameSignUpChange(userNameSignUp:String){
-        loginUiState = loginUiState.copy(userNameSignUp = userNameSignUp)
+        Log.d("LoginViewModel", "onUserNameSignUpChange called with: $userNameSignUp")
+        loginUiState.value = loginUiState.value.copy(userNameSignUp = userNameSignUp)
     }
 
     fun onPasswordSignUpChange(passwordSignUp:String){
-        loginUiState = loginUiState.copy(passwordSignUp = passwordSignUp)
+        Log.d("LoginViewModel", "onPasswordSignUpChange called with: $passwordSignUp")
+        loginUiState.value = loginUiState.value.copy(passwordSignUp = passwordSignUp)
     }
 
     fun onConfirmPasswordSignUpChange(confirmPasswordSignUp:String){
-        loginUiState = loginUiState.copy(confirmPasswordSignUp = confirmPasswordSignUp)
+        Log.d("LoginViewModel", "onConfirmPasswordSignUpChange called with: $confirmPasswordSignUp")
+        loginUiState.value = loginUiState.value.copy(confirmPasswordSignUp = confirmPasswordSignUp)
     }
 
-    private fun validateLoginForm() =
-        loginUiState.userName.isBlank() &&
-                loginUiState.password.isBlank()
+    private fun validateLoginForm(): Boolean {
+        Log.d("LoginViewModel", "validateLoginForm called.")
+        return loginUiState.value.userName.isNotBlank() &&
+                loginUiState.value.password.isNotBlank()
+    }
 
-    private fun validateSignUpForm() =
-        loginUiState.userNameSignUp.isBlank() &&
-                loginUiState.passwordSignUp.isBlank() &&
-                loginUiState.confirmPasswordSignUp.isBlank()
+    private fun validateSignUpForm(): Boolean {
+        Log.d("LoginViewModel", "validateSignUpForm called.")
+        return loginUiState.value.userNameSignUp.isBlank() &&
+                loginUiState.value.passwordSignUp.isBlank() &&
+                loginUiState.value.confirmPasswordSignUp.isBlank()
+    }
 
     fun createUser(context: Context) = viewModelScope.launch{
+            Log.d("LoginViewModel", "createUser called.")
         try{
             if(!validateSignUpForm()){
                 throw Exception("Please fill in all the fields")
             }
-            loginUiState = loginUiState.copy(isLoading = true)
-            if(loginUiState.passwordSignUp != loginUiState.confirmPasswordSignUp){
+            loginUiState.value = loginUiState.value.copy(isLoading = true)
+            if(loginUiState.value.passwordSignUp != loginUiState.value.confirmPasswordSignUp){
                 throw Exception("Passwords do not match")
             }
-            loginUiState = loginUiState.copy(signUpError = null)
+            loginUiState.value = loginUiState.value.copy(signUpError = null)
             repository.createUser(
-                loginUiState.userNameSignUp,
-                loginUiState.passwordSignUp
+                loginUiState.value.userNameSignUp,
+                loginUiState.value.passwordSignUp
             ){ isSuccessful ->
                 if(isSuccessful){
                     Toast.makeText(context, "Success Login", Toast.LENGTH_SHORT).show()
-                    loginUiState = loginUiState.copy(isSuccessLogin = true)
+                    loginUiState.value = loginUiState.value.copy(isSuccessLogin = true)
 
                 }else{
                     Toast.makeText(context, "Failed Login", Toast.LENGTH_SHORT).show()
-                    loginUiState = loginUiState.copy(isSuccessLogin = false)
+                    loginUiState.value = loginUiState.value.copy(isSuccessLogin = false)
                 }
             }
 
         } catch (e:Exception){
-            loginUiState = loginUiState.copy(signUpError = e.localizedMessage)
+            loginUiState.value = loginUiState.value.copy(signUpError = e.localizedMessage)
         } finally {
-            loginUiState = loginUiState.copy(isLoading = false)
+            loginUiState.value = loginUiState.value.copy(isLoading = false)
         }
     }
 
     fun loginUser(context: Context) = viewModelScope.launch{
+        Log.d("LoginViewModel", "loginUser called.")
         try{
             if(!validateLoginForm()){
                 throw Exception("Please fill in all the fields")
             }
-            loginUiState = loginUiState.copy(isLoading = true)
-            loginUiState = loginUiState.copy(loginError = null)
+            loginUiState.value = loginUiState.value.copy(isLoading = true)
+            loginUiState.value = loginUiState.value.copy(loginError = null)
             repository.login(
-                loginUiState.userName,
-                loginUiState.password
+                loginUiState.value.userName,
+                loginUiState.value.password
             ){ isSuccessful ->
                 if(isSuccessful){
                     Toast.makeText(context, "Success Login", Toast.LENGTH_SHORT).show()
-                    loginUiState = loginUiState.copy(isSuccessLogin = true)
+                    loginUiState.value = loginUiState.value.copy(isSuccessLogin = true)
 
                 }else{
                     Toast.makeText(context, "Failed Login", Toast.LENGTH_SHORT).show()
-                    loginUiState = loginUiState.copy(isSuccessLogin = false)
+                    loginUiState.value = loginUiState.value.copy(isSuccessLogin = false)
                 }
             }
 
         } catch (e:Exception){
-            loginUiState = loginUiState.copy(loginError = e.localizedMessage)
+            loginUiState.value = loginUiState.value.copy(loginError = e.localizedMessage)
         } finally {
-            loginUiState = loginUiState.copy(isLoading = false)
+            loginUiState.value = loginUiState.value.copy(isLoading = false)
         }
     }
 
