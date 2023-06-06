fb535d9b17b179a37a484daf7b9d8ad99e4648a7
ValentinHerzog <valentin.herzog@hotmail.com>
2023-06-06 13:28:46 +0200

    Auth -> Sign Up, Login, Test home page, viewmodel

---
+++ ENSIHub/app/src/main/java/com/example/ensihub/ui/screens/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.ensihub.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Home(){
+    Text(text = "TEST TEST TEST")
+}
---
+++ ENSIHub/app/src/main/java/com/example/ensihub/ui/screens/Navigation.kt
@@ -0,0 +1,71 @@
+package com.example.ensihub.ui.screens
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.ensihub.login.LoginViewModel
+
+enum class LoginRoutes{
+    Signup,
+    Signin
+}
+
+enum class HomeRoutes{
+    Home,
+    Profile,
+    Settings
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun Navigation(
+    navController: NavHostController = rememberNavController(),
+    loginViewModel: LoginViewModel
+){
+    NavHost(navController = navController, startDestination = LoginRoutes.Signin.name){
+        composable(route = LoginRoutes.Signin.name){
+            LoginScreen(onNavToHomePage = {
+                navController.navigate(HomeRoutes.Home.name){
+                    launchSingleTop = true
+                    popUpTo(LoginRoutes.Signin.name){
+                        inclusive = true
+                    }
+                }
+            }) {
+                navController.navigate(LoginRoutes.Signup.name){
+                    launchSingleTop = true
+                    popUpTo(LoginRoutes.Signin.name){
+                        inclusive = true
+                    }
+                }
+            }
+
+        }
+
+        composable(route = LoginRoutes.Signup.name) {
+            SignUpScreen(onNavToHomePage = {
+                navController.navigate(HomeRoutes.Home.name) {
+                    popUpTo(LoginRoutes.Signup.name) {
+                        inclusive = true
+                    }
+                }
+            }) {
+                navController.navigate(LoginRoutes.Signin.name) {
+                    popUpTo(LoginRoutes.Signup.name) {
+                    }
+                }
+            }
+        }
+
+        composable(route = HomeRoutes.Home.name){
+            Home()
+        }
+
+    }
+}
