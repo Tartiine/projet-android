9154c48ac77c64a17ddc9aac005dfe57f17beecc
Thib <thibaut.herault@gmail.com>
2023-06-06 13:28:12 +0200

    Starting Feed class to load data from firestore

--- ENSIHub/app/src/main/java/com/example/ensihub/back/Feed.kt
+++ ENSIHub/app/src/main/java/com/example/ensihub/back/Feed.kt
@@ -2,36 +2,77 @@ package com.example.ensihub.back
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import com.example.ensihub.Post
+import com.google.firebase.firestore.Filter
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class Feed {
-    private val data = mutableListOf<String>()
+    private val posts = mutableListOf<Post>()
     private val db = Firebase.firestore
-    private val i = 10
+    private var i: Long = 10
 
     init {
-        db.collection("posts").get()
+        db.collection("posts").limit(10).get()
             .addOnSuccessListener { result ->
                 for (document in result) {
                     Log.d(TAG, "${document.id} => ${document.data}")
-
+                    val data = document.data
+                    posts.add(Post(data["id"] as String, data["text"] as String, data["timestamp"] as Long, data["author"] as String, data["likesCount"] as Int))
                 }
             }
             .addOnFailureListener { exception ->
                 Log.w(TAG, "Error getting documents.", exception)
             }
-         }
-
-    fun load(number: Int) {
     }
 
-    fun getData(): MutableList<String> {
-        return this.data
+    fun loadMore() {
+        i += 10
+        db.collection("posts").limit(i).whereNotIn("id", posts).get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    val data = document.data
+                    posts.add(Post(data["id"] as String, data["text"] as String, data["timestamp"] as Long, data["author"] as String, data["likesCount"] as Int))
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 
-    fun search(key: String): List<String> {
-        return this.data.filter { p ->  p.user.username == key || p.text?.contains(key) == true }
+    fun reload() {
+        i = 10
+        posts.clear()
+        db.collection("posts").limit(i).get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    val data = document.data
+                    posts.add(Post(data["id"] as String, data["text"] as String, data["timestamp"] as Long, data["author"] as String, data["likesCount"] as Int))
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+    }
+
+    fun addPost(post: Post) {
+        db.collection("posts").add(post)
+            .addOnSuccessListener {
+                Log.d(TAG, "Successfully sent post: $it")
+            }
+            .addOnFailureListener {
+                Log.w(TAG, "Error while sending post: $it")
+            }
+    }
+
+    fun getData(): MutableList<Post> {
+        return this.posts
+    }
+
+    fun search(key: String): List<Post> {
+        return this.posts.filter { p ->  p.id == key || p.text?.contains(key) == true }
     }
 
 }
