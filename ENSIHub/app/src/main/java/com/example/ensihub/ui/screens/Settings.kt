1810b12e7a79bacff0632b5afea41e96bcb3a221
Thib <thibaut.herault@gmail.com>
2023-06-07 15:11:31 +0200

    Mise à jour de Settings

--- ENSIHub/app/src/main/java/com/example/ensihub/ui/screens/Settings.kt
+++ ENSIHub/app/src/main/java/com/example/ensihub/ui/screens/Settings.kt
@@ -1,23 +1,45 @@
 package com.example.ensihub.ui.screens
 
+import androidx.compose.material3.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExtendedFloatingActionButton
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.Group
+import androidx.compose.ui.graphics.vector.ImageVector
+
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.Popup
 import com.example.ensihub.MainClasses.Role
 import com.example.ensihub.MainClasses.User
 import com.example.ensihub.R
@@ -26,19 +48,51 @@ import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun SettingsView(user: User) {
+    var popupControl = remember {
+        mutableStateOf(false)
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.DarkGray)
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
-            ExtendedFloatingActionButton(
-                onClick = { disconnect }
-            )
+
+            SettingsGroup(name = "About") {
+                SettingsTextComp(icon = Icons.Filled.Person, name = "test")
+                SettingsTextComp(icon = Icons.Filled.Build, name = "Build")
+            }
+            Button (
+                shape = RoundedCornerShape(10.dp),
+                onClick = { popupControl.value = true },
+                colors = ButtonDefaults.buttonColors(Color.Red)
+            ) {
+                Text(text = "Logout", Modifier.padding(start = 10.dp), style = MaterialTheme.typography.bodyLarge)
+            }
+            if (popupControl.value) {
+                AlertDialog(shape = RoundedCornerShape(8.dp),
+                    onDismissRequest = {
+                    popupControl.value = false
+                }, confirmButton = {
+                    Button(onClick = { disconnect() }) {
+                        Text("Confirm", color = Color.Red)
+                    }
+                }, title = {
+                    Text(text = "Are you sure you want to disconnect?")
+                }, dismissButton = {
+                        Button(onClick = { popupControl.value = false }) {
+                            Text("Cancel", color = Color.Red)
+                        }
+                })
+            }
             Text(
                 text = "ENSIHub",
                 textAlign = TextAlign.Center,
@@ -52,7 +106,7 @@ fun SettingsView(user: User) {
                 color = Color.White
             )
             Text(
-                text = "Developed by "
+                text = "Developed by Ensisoft"
             )
             Text(
                 text = "MIT License"
@@ -63,6 +117,7 @@ fun SettingsView(user: User) {
 }
 
 
+
 @Preview
 @Composable
 fun SettingsViewPreview() {
@@ -71,5 +126,60 @@ fun SettingsViewPreview() {
 
 fun disconnect() {
     FirebaseAuth.getInstance().signOut()
-    R.string.
+
+}
+
+@Composable
+fun SettingsGroup(
+    name: String,
+    // to accept only composables compatible with column
+    content: @Composable ColumnScope.() -> Unit ){
+    Column(modifier = Modifier.padding(vertical = 8.dp)) {
+        Text(name, color = Color.White)
+        Spacer(modifier = Modifier.height(8.dp))
+        Surface(
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(4),
+        ) {
+            Column {
+                content()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun SettingsTextComp(
+    icon: ImageVector,
+    name: String,
+) {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    ) {
+        Column(modifier = Modifier.background(MaterialTheme.colorScheme.onSurface)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+            ) {
+                Icon(imageVector = icon, contentDescription = null, tint = Color.White)
+                Spacer(modifier = Modifier.width(16.dp))
+                Column(modifier = Modifier.padding(8.dp)) {
+                    // setting text title
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Start,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+            Divider()
+        }
+    }
 }
