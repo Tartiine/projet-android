997dfd10dcab6c94bccaeffb932610510c67d8ea
Mahmoud <mahmoudkhoubji25@gmail.com>
2023-06-08 14:54:13 +0200

    Settings screen

---
+++ ENSIHub/app/src/main/java/com/example/ensihub/ui/screens/settings/SettingsFonts.kt
@@ -0,0 +1,9 @@
+package com.example.ensihub.ui.screens.settings
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.example.ensihub.R
+
+val roboto: FontFamily = FontFamily.Default
