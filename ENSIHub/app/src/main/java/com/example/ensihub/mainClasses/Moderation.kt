b2c33eee5625f10a1de73d28922958ff92d05506
FrancoisROYuha <franroy68@gmail.com>
2023-06-12 10:41:56 +0200

    trying to not blow-up the project

---
+++ ENSIHub/app/src/main/java/com/example/ensihub/mainClasses/Moderation.kt
@@ -0,0 +1,44 @@
+package com.example.ensihub.mainClasses
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class Moderation {
+    private val db = Firebase.firestore
+
+    fun approvePost(post : Post) {
+        db.collection("posts").document(post.id.toString()).update("status", PostStatus.APPROVED.name)
+            .addOnSuccessListener {
+                Log.d(TAG, "Post approved and can be load in the feed")
+            }
+            .addOnFailureListener{ exception ->
+                Log.w(TAG, "Error approving post", exception)
+            }
+    }
+
+    fun rejectPost(post : Post) {
+        db.collection("posts").document(post.id.toString()).delete()
+            .addOnSuccessListener {
+                Log.d(TAG, "Post rejected and deleted from the database")
+            }
+            .addOnFailureListener{ exception ->
+                Log.w(TAG, "Error rejecting post", exception)
+            }
+    }
+
+    fun getPendingPosts() : MutableList<Post> {
+        val pendingPosts = mutableListOf<Post>()
+        db.collection("posts").whereEqualTo("status", PostStatus.PENDING.name).get()
+            .addOnSuccessListener { result ->
+                for(document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                    val data = document.data
+                    val post = Post(data["id"] as Long, data["text"] as String, data["timestamp"] as Long, data["author"] as String, data["likesCount"] as Long, data["imageUrl"] as String?, data["videoUrl"] as String?, data["status"] as PostStatus)
+                    pendingPosts.add(post)
+                }
+            }
+        return pendingPosts
+    }
+}
