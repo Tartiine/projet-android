d9d0c43db5d44548b6457654a620ade456e2d012
FrancoisROYuha <franroy68@gmail.com>
2023-06-13 11:15:34 +0200

    Update for moderation features

--- ENSIHub/app/src/main/java/com/example/ensihub/ui/screens/ModerationScreen.kt
+++ ENSIHub/app/src/main/java/com/example/ensihub/ui/screens/ModerationScreen.kt
@@ -1,10 +1,10 @@
 package com.example.ensihub.ui.screens
 
-
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -26,6 +26,7 @@ import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,25 +34,25 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
-import com.example.ensihub.mainClasses.FeedViewModel
 import com.example.ensihub.mainClasses.Moderation
 import com.example.ensihub.mainClasses.Post
 import com.example.ensihub.mainClasses.Role
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun PostView(
+fun ModerationView(
     post: Post,
     showImage: Boolean,
     onToggleShowImage: () -> Unit,
-    viewModel: FeedViewModel
+    moderationViewModel : Moderation
 ) {
-
-    val isLiked = remember { mutableStateOf(false) }  // Remember the liked state
-
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -73,35 +74,10 @@ fun PostView(
                 modifier = Modifier.padding(top = 4.dp)
             )
         }
+
         Divider()
 
-
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Button(
-                modifier = Modifier.size(60.dp),
-                colors = ButtonDefaults.buttonColors(contentColor = if (isLiked.value) Color(247, 152,23) else Color.White, backgroundColor = Color.Transparent),
-                onClick = {
-                    isLiked.value = !isLiked.value
-                    if (isLiked.value) {
-                        viewModel.likePost(post)
-                        Log.d("PostView", "likePost clicked for postId = ${post.id}")
-                    } else {
-                        viewModel.unlikePost(post)
-                        Log.d("PostView", "unlikePost clicked for postId = ${post.id}")
-                    }
-                },
-                elevation = null
-            ) {
-                Icon(if (isLiked.value) Icons.Filled.ThumbUp else Icons.Outlined.ThumbUp, null)
-            }
-
-            Text(
-                text = "Likes: ${post.likesCount}",
-                style = MaterialTheme.typography.body1,
-                color = Color.White,
-                modifier = Modifier.padding(start = 8.dp, end = 20.dp)
-            )
-
             Spacer(modifier = Modifier.width(8.dp))
             if (post.imageUrl?.isNotEmpty() == true) {
                 Row(modifier = Modifier.clickable { onToggleShowImage() }) {
@@ -124,48 +100,31 @@ fun PostView(
             }
         }
 
-        if(user.role==Role.USER){
-            Spacer(modifier = Modifier.width(8.dp))
-            Button(
-                modifier = Modifier.width(80.dp),
-                onClick = {
-                    /* On click function */
-                    Log.d("PostView", "reportPost clicked for postId = ${post.id}")
-                },
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF213865)),
-                elevation = null
-            ) {
-                Text("Report")
-            }
-        }else{
-            Button(
-                modifier = Modifier.width(80.dp),
-                onClick = {
-
-                    Log.d("PostView", "validatePost clicked for postId = ${post.id}")
-                },
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green),
-                elevation = null
-            ) {
-                Text("Validate")
-            }
-            Button(
-                modifier = Modifier.width(80.dp),
-                onClick = {
-
-                    Log.d("PostView", "deletePost clicked for postId = ${post.id}")
-                },
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
-                elevation = null
-            ) {
-                Text("Delete")
-            }
-
+        Button(
+            modifier = Modifier.width(80.dp),
+            onClick = {
+                moderationViewModel.approvePost(post)
+                Log.d("ModerationView", "The post = ${post.id} is approved")
+            },
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Green),
+            elevation = null
+        ) {
+            Text("Approve")
         }
 
+        Button(
+            modifier = Modifier.width(80.dp),
+            onClick = {
+                moderationViewModel.rejectPost(post)
+                Log.d("ModerationView", "The post = ${post.id} is rejected and deleted")
+            },
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
+            elevation = null
+        ) {
+            Text("Reject")
+        }
 
         Divider()
 
@@ -176,24 +135,19 @@ fun PostView(
     }
 }
 
-
-
-
 @Preview
 @Composable
-fun PostViewPreview() {
-    val viewModel: FeedViewModel = viewModel()
-    val showImage = remember { mutableStateOf(true) }
-    PostView(
+fun ModerationViewPreview() {
+    val moderation = Moderation()
+    val showImage = remember { mutableStateOf(true)}
+    ModerationView(
         post = Post(
-            text = "body",
-            author = "titi\n difdc,",
+            text = "Sometimes my genius is almost frightening",
+            author = "My\n Self,",
             imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmacollectiondepoupees.fr%2F2019%2F05%2F05%2Fhistorique-kiki-sekiguchi-ajena-monchhichi%2F&psig=AOvVaw3jXWSqFvxRp2mjB5uxrn7u&ust=1686322016766000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPDCh5n1s_8CFQAAAAAdAAAAABAD"
         ),
         showImage = showImage.value,
         onToggleShowImage = { showImage.value = !showImage.value },
-        viewModel = viewModel
+        moderationViewModel = moderation
     )
 }
-
-
